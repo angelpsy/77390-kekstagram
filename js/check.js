88a172ea74660c6d160827ef7cb2fc37c1d8f1f6
angelpsy <angelpsymail@gmail.com>
2016-01-23 15:57:54 +0500

    Исправлено в соответствии с замечаниями

--- js/check.js
+++ js/check.js
@@ -4,41 +4,31 @@ var getMessage = function (a, b) {
 
     switch (typeof(a)) {
         case 'boolean':
-            {
                 if (a) {
                     return 'Переданное GIF-изображение анимировано и содержит ' + b + ' кадров';
                 } else {
                     return 'Переданное GIF-изображение не анимировано';
                 }
-                break
-            }
+                break;
         case 'number':
-            {
                 return 'Переданное SVG-изображение содержит ' + a + ' объектов и ' + b * 4 + ' аттрибутов';
-                break
-            }
+                break;
         case 'object':
-            {
                 if ((a instanceof Array) && (b instanceof Array)) {
                     var square = 0;
                     a.forEach(function(item, i) {
-                    	if (!b[i]) { 
-                    		return;
-                    	};
                         square += item * b[i];
                     });
                     return 'Общая площадь артефактов сжатия: ' + square + ' пикселей';
-                }
-                if (a instanceof Array) {
+                } else if (a instanceof Array) {
                     var sum = 0;
                     sum = a.reduce(function(val, currentItem) {
                         val += currentItem;
                         return val;
                     });
                     return 'Количество красных точек во всех строчках изображения: ' + sum;
-                }
-                break
-            }
+                };
+                break;
     }
     return "непонятно";
 }
