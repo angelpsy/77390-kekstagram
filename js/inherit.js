9a1c9416c8ed20e5cf09374111b1d2404b5308d9
angelpsy <angelpsymail@gmail.com>
2016-03-02 20:27:09 +0500

    правки оформления кода

--- js/inherit.js
+++ js/inherit.js
@@ -1,13 +1,13 @@
 'use strict';
 
-  var inherit = function(FuncChild, FuncParent) {
-    if (typeof FuncChild !== 'function' || typeof FuncParent !== 'function') {
-      console.log('переданы не функции');
-      return;
-    }
-    var Empty = function() {};
-    Empty.prototype = FuncParent.prototype;
-    FuncChild.prototype = new Empty();
-  };
+var inherit = function(FuncChild, FuncParent) {
+  if (typeof FuncChild !== 'function' || typeof FuncParent !== 'function') {
+    console.log('переданы не функции');
+    return;
+  }
+  var Empty = function() {};
+  Empty.prototype = FuncParent.prototype;
+  FuncChild.prototype = new Empty();
+};
 
 module.exports = inherit;
--- js/photo.js
+++ js/photo.js
@@ -1,63 +1,63 @@
 'use strict';
 
-  var _IMG_SIZE = 182;
-  var _pictureTemplate = document.getElementById('picture-template');
+var _IMG_SIZE = 182;
+var _pictureTemplate = document.getElementById('picture-template');
 
-  if (!('content' in _pictureTemplate)) {
-    _pictureTemplate.content = _pictureTemplate;
-  }
+if (!('content' in _pictureTemplate)) {
+  _pictureTemplate.content = _pictureTemplate;
+}
 
-  var _pictureTemplatSelector = _pictureTemplate.content.querySelector('.picture');
-  var Photo = function(data) {
-    this._data = data;
-  };
+var _pictureTemplatSelector = _pictureTemplate.content.querySelector('.picture');
+var Photo = function(data) {
+  this._data = data;
+};
 
-  Photo.prototype.render = function() {
-    this.element = _pictureTemplatSelector.cloneNode(true);
-    this.element.querySelector('.picture-comments').textContent = this._data.comments;
-    this.element.querySelector('.picture-likes').textContent = this._data.likes;
-    this.element.href = this._data.url;
+Photo.prototype.render = function() {
+  this.element = _pictureTemplatSelector.cloneNode(true);
+  this.element.querySelector('.picture-comments').textContent = this._data.comments;
+  this.element.querySelector('.picture-likes').textContent = this._data.likes;
+  this.element.href = this._data.url;
 
-    var newImage = new Image();
-    newImage.onload = function() {
-      clearTimeout(imgLoadTimeout);
-      this.element.replaceChild(newImage, this.element.querySelector('img'));
-    }.bind(this);
-    newImage.onerror = function() {
+  var newImage = new Image();
+  newImage.onload = function() {
+    clearTimeout(imgLoadTimeout);
+    this.element.replaceChild(newImage, this.element.querySelector('img'));
+  }.bind(this);
+  newImage.onerror = function() {
+    this.element.classList.add('picture-load-failure');
+  }.bind(this);
+  newImage.src = this._data.url;
+
+  newImage.width = _IMG_SIZE;
+  newImage.height = _IMG_SIZE;
+
+
+  var IMG_TIMEOUT = 10000,
+    imgLoadTimeout = setTimeout(function() {
+      newImage.src = '';
       this.element.classList.add('picture-load-failure');
-    }.bind(this);
-    newImage.src = this._data.url;
+    }.bind(this), IMG_TIMEOUT);
 
-    newImage.width = _IMG_SIZE;
-    newImage.height = _IMG_SIZE;
+  this.element.addEventListener('click', function(evt) {
+    event.preventDefault();
+    if (event.target.tagName === 'IMG' &&
+        !evt.target.parentNode.classList.contains('picture-load-failure') &&
+        typeof this.onClick === 'function') {
+      this.onClick();
+    }
+  }.bind(this));
+};
 
+Photo.prototype.getImgSize = function() {
+  return _IMG_SIZE;
+};
 
-    var IMG_TIMEOUT = 10000,
-      imgLoadTimeout = setTimeout(function() {
-        newImage.src = '';
-        this.element.classList.add('picture-load-failure');
-      }.bind(this), IMG_TIMEOUT);
+Photo.prototype.onClick = null;
 
-    this.element.addEventListener('click', function(evt) {
-      event.preventDefault();
-      if (event.target.tagName === 'IMG' &&
-          !evt.target.parentNode.classList.contains('picture-load-failure') &&
-          typeof this.onClick === 'function') {
-        this.onClick();
-      }
-    }.bind(this));
-  };
-
-  Photo.prototype.getImgSize = function() {
-    return _IMG_SIZE;
-  };
-
-  Photo.prototype.onClick = null;
-
-  Photo.prototype.remove = function() {
-    this.onClick = null;
-    this.element.removeEventListener('click', this._onClick);
-    this.element.parentNode.removeChild(this.element);
-  }
+Photo.prototype.remove = function() {
+  this.onClick = null;
+  this.element.removeEventListener('click', this._onClick);
+  this.element.parentNode.removeChild(this.element);
+};
 
 module.exports = Photo;
